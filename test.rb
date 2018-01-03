1041fd4269e8ef3cdd1f45997e58839ad6e232d3
jessmitch42 <jessicamitch42@gmail.com>
2018-01-03 12:22:03 -0500

    examples fixed

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ arr = [1, 2, 3, 4, 5]
 
 new_arr = arr
 
-puts new_arr # 1 2 3 4 5
+puts arr # 1 2 3 4 5
 
-arr = arr.collect!{ |v| v + 1 }
+new_arr = [1, 2, 3, 4, 5].collect{ |v| v + 1 }
 
-puts new_arr # 2 3 4 5 6
+puts arr # 1 2 3 4 5
